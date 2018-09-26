4f617752f125e8335bbe73560cac109d378f8c39
Guilherme Reginaldo Ruella <potentii@gmail.com>
2018-09-25 22:28:06 -0300

    Charset option
    - now it's possible to configure a charset to be used in the IPC communication

--- src/main/java/com/potentii/Main.java
+++ src/main/java/com/potentii/Main.java
@@ -2,9 +2,11 @@ package com.potentii;
 
 import com.potentii.ipc.worker.api.IPCWorker;
 
+import java.io.IOException;
+
 public class Main{
 	
-	public static void main(String[] args){
+	public static void main(String[] args) throws IOException {
 		IPCWorker bridge = new IPCWorker(System.in, System.out);
 		
 		bridge.listen((req, res) -> {
--- src/main/java/com/potentii/ipc/worker/api/IPCWorker.java
+++ src/main/java/com/potentii/ipc/worker/api/IPCWorker.java
@@ -1,7 +1,10 @@
 package com.potentii.ipc.worker.api;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintStream;
+import java.nio.charset.IllegalCharsetNameException;
+import java.nio.charset.UnsupportedCharsetException;
 import java.util.Scanner;
 import java.util.regex.Pattern;
 
@@ -22,6 +25,7 @@ public class IPCWorker{
 	private boolean listening;
 
 
+
 	/**
 	 * The stream from which the messages will be received
 	 */
@@ -32,6 +36,12 @@ public class IPCWorker{
 	 */
 	@Nullable
 	private final PrintStream target;
+	/**
+	 * The charset to read/write messages
+	 */
+	@NotNull
+	private final Charset charset;
+
 
 
 	/**
@@ -57,15 +67,33 @@ public class IPCWorker{
 	 *                  requests to (e.g. In most cases it will be {@code System.in})
 	 * @param target The destination of the messages, that is, the stream that the other process will read the
 	 *                  responses from (e.g. In most cases it will be {@code System.out})
+	 * @param charset The charset to use (default: UTF-8)
 	 */
-	public IPCWorker(@NotNull InputStream source, @Nullable PrintStream target) {
+	public IPCWorker(@NotNull InputStream source, @Nullable PrintStream target, @Nullable Charset charset) {
 		super();
 		if(source == null)
 			throw new IllegalArgumentException("The source stream must not be null");
 
 		this.source = source;
 		this.target = target;
+		this.charset = (charset == null)
+			? Charset.UTF8
+			: charset;
 	}
+
+
+	/**
+	 * Prepares a new inter-process communication bridge, using UTF-8 as the default charset
+	 * To start the communication flow, use the {@code listen(MessageHandler)} method of the returned instance
+	 * @param source The origin of the messages, that is, the stream that the other process will write the
+	 *                  requests to (e.g. In most cases it will be {@code System.in})
+	 * @param target The destination of the messages, that is, the stream that the other process will read the
+	 *                  responses from (e.g. In most cases it will be {@code System.out})
+	 */
+	public IPCWorker(@NotNull InputStream source, @Nullable PrintStream target) {
+		this(source, target, Charset.UTF8);
+	}
+
 
 
 	/**
@@ -103,19 +131,19 @@ public class IPCWorker{
 	 * @param requestHandler A handler implementation that should process the received request and change the response
 	 *                          to be sent back to the master process
 	 */
-	public void listen(@Nullable final MessageHandler requestHandler){
+	public void listen(@Nullable final MessageHandler requestHandler) throws IOException {
 		// *Initializing the processor of the requests that will be received, using the given handler implementation:
 		final RequestProcessor requestProcessor = new RequestProcessor(requestHandler);
 		// *Initializing the unit that will be responsible to process each raw message:
 		final Processor<String, String> messageProcessor = new MessageProcessor(requestProcessor);
 		
 		// *Setting up the scanner that will wait for messages on the 'source' stream:
-		final Scanner scanner = new Scanner(source);
+		final Scanner scanner = new Scanner(source, charset.getCharsetName());
 		// *Using the default delimiter rule, to separate each of the raw messages:
 		scanner.useDelimiter(MESSAGE_DELIMITER_REGEX);
 
 		// *Signaling that this worker process is ready to receive requests:
-		target.print(PROCESS_ALIVE);
+		send(PROCESS_ALIVE);
 
 		// *Signaling that this bridge have started listening to requests:
 		listening = true;
@@ -127,10 +155,32 @@ public class IPCWorker{
 			final String inputData = scanner.next();
 
 			// *Processing the received input:
-			target.print(processInput(inputData, messageProcessor));
+			final String response = processInput(inputData, messageProcessor);
+
+			// *Sending the response:
+			send(response);
 		}
 	}
 
+
+	private byte[] send(String text) throws IOException {
+		final byte[] rawText = convertToCharset(text, charset);
+		target.write(rawText);
+		return rawText;
+	}
+
+
+	@NotNull
+	private byte[] convertToCharset(String text, Charset charset){
+		if(text == null)
+			return new byte[]{};
+
+		try{
+			return text.getBytes(java.nio.charset.Charset.forName(charset.getCharsetName()));
+		} catch(IllegalCharsetNameException | UnsupportedCharsetException | NullPointerException e){
+			return new byte[]{};
+		}
+	}
 
 
 	private void insertStopSignalToSource(final InputStream source){
@@ -153,11 +203,29 @@ public class IPCWorker{
 		return listening;
 	}
 
+	@NotNull
 	public InputStream getSource() {
 		return source;
 	}
 
+	@Nullable
 	public PrintStream getTarget() {
 		return target;
+	}
+
+
+
+	public static enum Charset{
+		UTF8("UTF-8");
+
+		private String charsetName;
+
+		Charset(String charsetName) {
+			this.charsetName = charsetName;
+		}
+
+		public String getCharsetName() {
+			return charsetName;
+		}
 	}
 }
